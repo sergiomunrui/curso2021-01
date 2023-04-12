ef9efcf9e77026f5be573da89c24b5ea4ca22365
sergiomunrui <sergiomunrui@gmail.com>
2023-04-12 10:10:07 +0000

    Añadidas funciones y botones para campos calculados

--- helpdesk_angelmoya/models/helpdesk_ticket.py
+++ helpdesk_angelmoya/models/helpdesk_ticket.py
@@ -1,6 +1,6 @@
-from odoo import models, fields 
+from odoo import models, fields, api
 
-# ===========================================================================================
+# =====================================================================================================
 
 class HelpdeskTicketAction(models.Model):
     _name = 'helpdesk.ticket.action'
@@ -29,13 +29,15 @@ class HelpdeskTicketTag(models.Model):
         relation='helpdesk_ticket_tag_rel', # Este es el nombre de la tabla intermedia para relacionar los pares de registros en los Many2many
         column1='tag_id', # Este es el nombre de la columna de la tabla1 que queremos relacionar
         column2='ticket_id', # Este es el nombre de la columna de la tabla2 que queremos relacionar
-        string='Tickets')
+        string='Tags')
     
 # ===================================================================================================================  
 
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
     _description = 'Ticket' 
+    
+    # CAMPOS ONE2ONE, ONE2MANY Y MANY2MANY
     
     # Con este campo apuntamos al modelo de etiquetas (Tag) y vemos qué etiquetas tienen asociadas un determinado ticket
     tag_ids = fields.Many2many(
@@ -44,19 +46,20 @@ class HelpdeskTicket(models.Model):
         column1='ticket_id', # Este es el nombre de la columna de la tabla1 que queremos relacionar
         column2='tag_id', # Este es el nombre de la columna de la tabla2 que queremos relacionar
         string='Tags')
-    
-        
+          
     action_ids = fields.One2many(
         comodel_name='helpdesk.ticket.action',
         inverse_name='ticket_id',
         string='Actions')
     
-    # Hacemos un campo many to one para apuntar al usuario. Muchos apuntan a uno solo, en este caso apuntan a user_id
+    # Hacemos un campo many to one para apuntar al usuario. Muchos tickets pueden apuntar a uno solo, en este caso apuntan a user_id
     user_id = fields.Many2one(
         comodel_name='res.users', # res.users porque en link de la web aparece de esa forma cuando gestionamos usuarios
         string='Assigned to')
     
         
+    # CAMPOS HABITUALES
+    
     name = fields.Char(
         string='Name',
         required=True
@@ -69,8 +72,6 @@ class HelpdeskTicket(models.Model):
         string='Date'
     )
     
-    # - Estado del ticket, por defecto muestra nuevo si no le asignamos ninguno
-
     state = fields.Selection(
         [('nuevo', 'Nuevo'),
          ('asignado', 'Asignado'),
@@ -79,7 +80,7 @@ class HelpdeskTicket(models.Model):
          ('resuelto', 'Resuelto'),
          ('cancelado', 'Cancelado'),],
         string='State',
-        default='nuevo')
+        default='nuevo') # - Estado del ticket, por defecto muestra nuevo si no le asignamos ninguno
     
     # - Tiempo dedicado (en horas) 
     time = fields.Float(
@@ -104,22 +105,6 @@ class HelpdeskTicket(models.Model):
         string='Preventive Action',
         help='Descrive preventive actions to do')
     
-    # Es un campo de tipo muchos a uno porque muchos tickets pueden ser asignados a un solo usuario
-    user_id = fields.Many2one(
-        comodel_name='res.users',
-        string='Asignado a')
-    
-    # El campo nombre que sea obligatorio --> puesto arriba donde _name
-
-    # En algún campo añadir un texto de ayuda indicando su funcionalidad, luego revisar que funciona.  --> con el help hecho
-
-    # El campo Asignado:
-    # - hacer que sea solo de lectura --> hecho en el campo assigned
-    
-    
-    # EJERCICIO2
-    # Añadir en el header los siguiente botones:
-
     # - Asignar, cambia estado a asignado y pone a true el campo asignado, visible sólo con estado = nuevo        
     def asignar(self):
         self.ensure_one()
@@ -146,5 +131,33 @@ class HelpdeskTicket(models.Model):
     def cancelar(self):
         self.ensure_one()
         self.state = 'cancelado'
-
+        
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = self.user_id and True or False
+            
+    # Hacer un campo calculado que dentro de un ticket muestre la cantidad de tickets asociados al mismo user
+    
+    ticket_cantidad = fields.Integer(
+        string='Ticket Cantidad',
+        compute='_compute_ticket_cantidad') #le pasamos el resultado de la def _compute_ticket_cantidad
+    
+    @api.depends('user_id')
+    def _compute_ticket_cantidad (self):
+        for record in self:  # Iteramos sobre los registros que contiene el modelo en el que estamos
+            other_tickets = self.env['helpdesk.ticket'].search([('user_id', '=', record.user_id.id)]) # Buscamos los usuarios que coincidan con ese user_id y guardamos en un array
+            record.ticket_cantidad = len(other_tickets) # Guardamos en el campo ticket_cantidad el número de elementos del array other_tickets
+            
+    # crear un campo nombre de etiqueta, y hacer un botón que ree la nueva etiqeuta con ese nombre y lo asocie al ticket
+    tag_name = fields.Char(
+        string='Nombre Etiqueta'
+    )
+    
+    def create_tag(self):
+        self.ensure_one() # Porque va a ser un botón que va a estar dentro del formulario
+        self.write({ 
+            'tag_ids': [(0,0, {'name': self.tag_name})]           
+        })
+        self.tag_name = False
 
